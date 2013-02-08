a736b9bca0d578acbf631ae7ce0c2b592654b059
Mat Sadler <mat@redstudio.co.uk>
2013-02-08 12:36:19 +0000

    more typos

--- application/views/quotes/smiths/costs.php
+++ application/views/quotes/smiths/costs.php
@@ -6,14 +6,14 @@
 <ul>
 	<li><i class="icon-ok"></i> Custom Content Management System (£800)</li>
 	<li><i class="icon-ok"></i> Professional standards compliant responsive HTML5 design (£800)</li>
-	<li><i class="icon-ok"></i> About Page (£100)</li>
-	<li><i class="icon-ok"></i> Contact Page (£100)</li>
-	<li><i class="icon-ok"></i> News Page (£100)</li>
-	<li><i class="icon-ok"></i> Menu Page (£100)</li>
-	<li><i class="icon-ok"></i> Other basic pages (£100 each)</li>
+	<li><i class="icon-ok"></i> About Page (£150)</li>
+	<li><i class="icon-ok"></i> Contact Page (£150)</li>
+	<li><i class="icon-ok"></i> News Page (£150)</li>
+	<li><i class="icon-ok"></i> Menu Page (£150)</li>
+	<li><i class="icon-ok"></i> Other basic pages (£150 each)</li>
 </ul>
 
-<p>The site will include the facility to sign up to a email newsletter. An email marketing service such as <a target="_blank" href="http://www.mailchimp.com">Mailchimp</a> or similar
+<p>The site will include the facility to sign up to an email newsletter. A marketing service such as <a target="_blank" href="http://www.mailchimp.com">Mailchimp</a> or similar
 	is ideal for this, and can provide data on the success of email campaigns.</p>
 
 <p>We are also a Google Apps reseller, so if you haven't already got a professional email service in place we can set this up and provide one to one training in the services it
